c101e60f83a9d97f67a123d219382ad164914c33
Rene Lange <rine@rine.de>
2024-11-07 13:15:51 +0100

    work in progress api bug

--- custom_components/homeassistantedupage/__init__.py
+++ custom_components/homeassistantedupage/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import asyncio
 from datetime import timedelta
+import datetime
 from edupage_api.exceptions import BadCredentialsException, CaptchaException
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -52,16 +53,31 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     fetch_lock = asyncio.Lock() 
 
     async def fetch_data():
-        """function to fetch grade data."""
+        """Function to fetch grade and timetable data."""
         async with fetch_lock:
-
             try:
+                # Abrufen der Noten
                 grades_data = await edupage.get_grades()
-                _LOGGER.debug("grades_data: %s", grades_data)  # Zeigt die Daten im Log
-                return grades_data 
+                # _LOGGER.debug("grades_data: %s", grades_data)  # Zeigt die Noten im Log
+
+                # Abrufen des Stundenplans mit dem aktuellen Datum
+                current_date = datetime.date(2024, 11, 6)  # Beispielformat: "2024-11-06"
+                timetable_data = await edupage.get_my_timetable(current_date)
+
+                _LOGGER.debug("timetable_data: %s", timetable_data)  # Zeigt den Stundenplan im Log
+
+                # Kombiniere Noten und Stundenplan in einem Dictionary
+                return {
+                    "grades": grades_data,
+                    "timetable": timetable_data
+                }
+
             except Exception as e:
-                _LOGGER.error("error fetching grades data: %s", e)
-                return []
+                _LOGGER.error("error fetching data: %s", e)
+                return {
+                    "grades": [],
+                    "timetable": []
+                }
 
     coordinator = DataUpdateCoordinator(
         hass,
@@ -71,19 +87,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         update_interval=timedelta(minutes=5),
     )
 
-    # first data fetch
+    # First data fetch
     await asyncio.sleep(1)
     await coordinator.async_config_entry_first_refresh()
-    #await coordinator.async_request_refresh()
 
-    # save coordinator
+    # Save coordinator
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    # platforms forward
+    # Forward platforms
     await hass.config_entries.async_forward_entry_setups(entry, [Platform.SENSOR])
 
     return True
+
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload ConfigEntry."""
